0c8fc1e8ddc5b6fbb381e990bfa76f90ad746f1d
yuron3141 <sub2718-vrchat@yahoo.co.jp>
2022-12-26 21:51:48 +0900

    db-table-created1

---
+++ db/migrate/20221226120624_create_submits.rb
@@ -0,0 +1,17 @@
+class CreateSubmits < ActiveRecord::Migration[6.0]
+  def change
+    create_table :submits do |t|
+      t.string :ip_address, :null => false
+      t.integer :point_intention
+      t.integer :point_view
+      t.integer :point_expert
+      t.integer :point_tech
+      t.integer :generation
+      t.integer :sex , :default => 0
+      t.integer :month
+
+
+      t.timestamps
+    end
+  end
+end
